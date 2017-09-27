13b412a7104c58463322babbd63711eec29ef35b
Art Crawford <arthur.crawford@gmail.com>
2017-09-27 14:57:12 +0100

    Removed itest from osx build because docker not supported on travis osx

--- itest.sh
+++ itest.sh
@@ -2,7 +2,8 @@
 docker build -t com.a4pizza.raptly.itest src/itest
 docker rm -f com.a4pizza.raptly.itest >&/dev/null
 docker run --rm -d -p 9876:8080 --name com.a4pizza.raptly.itest com.a4pizza.raptly.itest serve
+# Wait in 1/10 second intervals for the aptly API to become available
 while ! curl -s http://localhost:9876/api/version | grep Version; do
-  sleep 0.1 # wait for 1/10 of the second before check again
+  sleep 0.1
 done
 PYTHONPATH=src/main/python pytest src/itest/python
