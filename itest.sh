439eaf130a32976451c607986deec85a2c9757d8
Art Crawford <arthur.crawford@gmail.com>
2017-09-27 14:15:28 +0100

    NOTICKET - Added sudo for docker commands

--- itest.sh
+++ itest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker build -t com.a4pizza.raptly.itest src/itest
-docker rm -f com.a4pizza.raptly.itest >&/dev/null
-docker run --rm -d -p 9876:8080 --name com.a4pizza.raptly.itest com.a4pizza.raptly.itest serve
+sudo docker build -t com.a4pizza.raptly.itest src/itest
+sudo docker rm -f com.a4pizza.raptly.itest >&/dev/null
+sudo docker run --rm -d -p 9876:8080 --name com.a4pizza.raptly.itest com.a4pizza.raptly.itest serve
 while ! curl -s http://localhost:9876/api/version | grep Version; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
