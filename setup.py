bcdf4bb8a9e7cf219e6564ad25bae248e5478159
Art Crawford <arthur.crawford@gmail.com>
2017-11-02 23:12:27 +0000

    [CI SKIP] moved main script to bin directory

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
-
 setup(
-    name='sample',
-    version='2.1.21',
+    name='raptly',
+    version='2.1.22',
     description='My description',
     long_description='long desc',
     author='Art Crawford',
     author_email='pizzaiolo@a4pizza.com',
     url='https://github.com/a4pizza/test1',
     license='license',
-    scripts=['src/main/python/raptly/raptly.py'],
+    scripts=['src/main/bin/raptly'],
     packages=find_packages('src/main/python'),
-    package_dir = { '': 'src/main/python' }
+    package_dir={'': 'src/main/python'}
 )
