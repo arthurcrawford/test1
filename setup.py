17eccecd7eddfa2948f387e8f34ea4a86509d89d
Art Crawford <arthur.crawford@gmail.com>
2017-11-02 14:27:39 +0000

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sample',
+    version='0.1.0',
+    description='My description',
+    long_description='long desc',
+    author='Art Crawford',
+    author_email='pizzaiolo@a4pizza.com',
+    url='https://github.com/a4pizza/test1',
+    license='license',
+    packages=find_packages('src/main/python')
+)
